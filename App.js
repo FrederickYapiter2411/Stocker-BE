69bce11f8c16eee268e23740fb2433a3ea19b275
FrederickYapiter2411 <165285425+FrederickYapiter2411@users.noreply.github.com>
2025-11-08 22:05:58 +0700

    Update App.js
    Fix: Update App.js

--- App.js
+++ App.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
     // origin: 'http://localhost:8080'
-    origin: '*'
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 
